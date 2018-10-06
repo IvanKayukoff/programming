a1a22660ed21e2405c08a1f92d8dcc033f70908d
Sky731 <Ivan4784@mail.ru>
2018-10-06 17:32:55 +0300

    add insertSelectTestWithPeople

--- server/src/test/kotlin/xyz/sky731/programming/lab8/SimpleORMTest.kt
+++ server/src/test/kotlin/xyz/sky731/programming/lab8/SimpleORMTest.kt
@@ -66,15 +66,27 @@ class SimpleORMTest {
     bredlam.people.add(Human("NintendoMan", 2000))
     bredlam.people.add(Human("Rogogit", 1000))
 
+    val bredlam2 = Bredlam("P3202")
+    bredlam2.people.add(Human("Kurman", 9000000))
+    bredlam2.people.add(Human("Andrew Memov", 0))
+
     orm.insert<Bredlam>(bredlam)
+    orm.insert<Bredlam>(bredlam2)
 
     val bredlams = orm.selectAll<Bredlam>().toList()
-    assertEquals(1, bredlams.size)
+    assertEquals(2, bredlams.size)
     assertEquals(2, bredlams[0].people.size)
     assertEquals("NintendoMan", bredlams[0].people[0].name)
     assertEquals(2000, bredlams[0].people[0].money)
     assertEquals("Rogogit", bredlams[0].people[1].name)
     assertEquals(1000, bredlams[0].people[1].money)
+
+    assertEquals(2, bredlams[1].people.size)
+    assertEquals("Kurman", bredlams[1].people[0].name)
+    assertEquals(9000000, bredlams[1].people[0].money)
+    assertEquals("Andrew Memov", bredlams[1].people[1].name)
+    assertEquals(0, bredlams[1].people[1].money)
   }
+
 
 }
